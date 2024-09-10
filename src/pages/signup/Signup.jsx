d9a3ffd28ea19d730d27ec9bf8fb33736b0d0331
marvellous <comlaf2001@gmail.com>
2024-09-10 10:53:55 +0100

    Sign in and Sign up UI done

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import style from './signUpStyle.module.css'
 
 function Signup() {
-  return <div>signup</div>
+  return (
+    <div className={style.body}>
+    <div className={style.left}>
+      <h2>Create Account</h2>
+      <div  className={style.login}>
+        <form action="" className={style.form}>
+          <div  className={style.name}>
+            <div  className={style.input}>
+              <label htmlFor="firstName">First name</label>
+              <br />
+              <input type="text" name='firstName' placeholder="Enter First name" required/>
+            </div>
+            <div  className={style.input}>
+              <label htmlFor="firstName">Last name</label>
+              <br />
+              <input type="text" name='lastName' placeholder="Enter Last name" required/>
+            </div>
+          </div>
+         <div className={style.name}> 
+          <div  className={style.input}>
+              <label htmlFor="email">Email</label>
+              <br />
+              <input type="email" name='email' placeholder="Enter email" required/>
+            </div>
+            <div   className={style.input}>
+              <label htmlFor="passowrd">Password</label>
+              <br />
+              <input type="password" name='password' placeholder="Enter password" required/>
+            </div>
+         </div>
+         <div className={style.submit}>
+          <button type='submit' className={style.button2}>SIGN UP</button>
+         </div>
+        </form>
+      </div>
+    </div>
+
+
+
+    <div className={style.right}>
+     <h2> Have an account?</h2>
+     <h3>Continue  your journey in one click</h3>
+     <a href="/"  className={style.toSignUp}>
+
+      <button className={style.button}>Sign In</button>
+     </a>
+    </div>
+
+  </div>
+  )
 }
 
 export default Signup

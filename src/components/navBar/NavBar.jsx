8eb4017150ea2d1eb125dc37478bea8162f0c28a
papasunne <asunday@skilltopempire.com>
2024-09-13 12:41:53 +0100

    Revert "update sidebar and navbar"
    This reverts commit 3c79adca4b6b1da34a166954a76ece36d74ad0de.

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,7 +1,29 @@
+import { BellIcon, CogIcon, Settings, User2 } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
-  return <div>NavBar</div>
+  return (
+    <div className="w-full fixed flex items-centes justify-between py-4 px-2 flex-wrap bg-gray-100">
+      <div className="flex gap-10 items-center">
+        <div>
+          <Link to="" className="text-gray-800">
+            Product/Logo
+          </Link>
+        </div>
+        <div>
+          <p className="text-imsLightPurple">Search</p>
+        </div>
+      </div>
+      <div className="flex gap-4 items-center">
+        <BellIcon size={20} className="text-imsPurple" />
+        <Settings size={20} className="text-imsPurple" />
+        <div className="w-8 h-8 rounded-full bg-imsDarkPurple flex justify-center items-center">
+          <User2 size={20} className="text-imsLightPurple" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NavBar

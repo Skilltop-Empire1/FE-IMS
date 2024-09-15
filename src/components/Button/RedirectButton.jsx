be00045e93a5052951495b05f5671e39d43001ad
marvellous <comlaf2001@gmail.com>
2024-09-15 15:56:54 +0100

    done

--- src/components/Button/RedirectButton.jsx
+++ src/components/Button/RedirectButton.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import style from './Button.module.css'
 
-function Button({ buttonName, onClick, className = '' }) {
+function RedirectButton({ buttonName, onClick, className = '',direction }) {
   const btnClassName = `${style.buttonName} ${className}`
   return (
+    <a href={direction}>
       <button className={btnClassName} onClick={onClick}>
         {buttonName}
       </button>
+    </a>
   )
 }
 
-export default Button
+export default RedirectButton

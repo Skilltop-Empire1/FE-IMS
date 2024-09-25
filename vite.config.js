f0d8c45fba030ab92f4ec40aa9ffa6a2ca52ea4d
marvellous <comlaf2001@gmail.com>
2024-09-25 14:18:51 +0100

    done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FE-IMS/"
-})
+  base: "/FE-IMS/",   // GitHub Pages base URL
+
+  build: {
+    rollupOptions: {
+      output: {
+        // Splitting node_modules packages into separate chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Creates a separate chunk for each node_module dependency
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    // Optionally, increase the chunk size limit if needed (default is 500kB)
+    chunkSizeWarningLimit: 1000,  // Increase this limit if your chunks are larger
+  },
+});
